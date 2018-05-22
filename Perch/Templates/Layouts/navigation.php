3544c1176d44171261c5c729212d3b3741757a7d
Will Moody <willmoody1@gmail.com>
2018-05-22 12:45:25 +0100

    Create navigation.php

---
+++ Perch/Templates/Layouts/navigation.php
@@ -0,0 +1,8 @@
+<nav role="navigation"><!-- navigation -->
+    <?php perch_pages_navigation(array(
+    'navgroup' =>'main',
+    'template' =>'main_item',
+    'hide-extensions' => true,
+    'levels' => 2)); ?>
+    <?php perch_search_form(); ?>
+</nav><!-- /navigation -->
